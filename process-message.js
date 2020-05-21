23a22a77c4f3ceb31ea4650f04b0d3684047b091
renan-77 <renanmonteiroft@gmail.com>
2020-05-21 04:55:26 +0100

    chatbot completely created, dealing with invalid city inputs due

---
+++ process-message.js
@@ -0,0 +1,79 @@
+const Dialogflow = require('dialogflow');
+const Pusher = require('pusher');
+const getWeatherInfo = require('./weather');
+const hotTemp = 25;
+const coldTemp = 15;
+
+    // You can find your project ID in your Dialogflow agent settings
+    const projectId = 'chatbot-juvywa'; //https://dialogflow.com/docs/agents#settings
+    const sessionId = '123456';
+    const languageCode = 'en-US';
+
+    const config = {
+      credentials: {
+        private_key: process.env.DIALOGFLOW_PRIVATE_KEY,
+        client_email: process.env.DIALOGFLOW_CLIENT_EMAIL,
+      },
+    };
+
+    const pusher = new Pusher({
+      appId: process.env.PUSHER_APP_ID,
+      key: process.env.PUSHER_APP_KEY,
+      secret: process.env.PUSHER_APP_SECRET,
+      cluster: process.env.PUSHER_APP_CLUSTER,
+      encrypted: true,
+    });
+
+    const sessionClient = new Dialogflow.SessionsClient(config);
+
+    const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+    const processMessage = message => {
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            text: message,
+            languageCode,
+          },
+        },
+      };
+
+      sessionClient
+        .detectIntent(request)
+        .then(responses => {
+          const result = responses[0].queryResult;
+
+           // If the intent matches 'detect-city'
+           if (result.intent.displayName === 'weather') {
+            const city = result.parameters.fields['cityName'].stringValue;
+
+            // fetch the temperature from openweather map
+            return getWeatherInfo(city).then(temperature => {
+                if(temperature >= hotTemp){
+                    return pusher.trigger('bot', 'bot-response', {
+                        message: `The weather in ${city} is ${temperature}°C, you can wear summing clothes :)`,
+                    });
+                }else if(temperature <= coldTemp){
+                    return pusher.trigger('bot', 'bot-response', {
+                        message: `The weather in ${city} is ${temperature}°C, you better wear a coat :(`,
+                    });
+                }else if(temperature > coldTemp && temperature < hotTemp){
+                    return pusher.trigger('bot', 'bot-response', {
+                        message: `The weather in ${city} is ${temperature}°C, it's mild, wear a shirt and go for a walk :)`,
+                    });
+                }
+              
+            });
+          }
+
+          return pusher.trigger('bot', 'bot-response', {
+            message: result.fulfillmentText,
+          });
+        })
+        .catch(err => {
+          console.error('ERROR:', err);
+        });
+    }
+
+    module.exports = processMessage;
